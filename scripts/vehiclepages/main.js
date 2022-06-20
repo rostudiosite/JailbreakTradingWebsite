909b5b44e5f9df13abdc53fa3b70d99bc527c011
cancle54 <103057044+cancle54@users.noreply.github.com>
2022-06-20 15:23:55 -0700

    RELEASE!!!!!!!

---
+++ scripts/vehiclepages/main.js
@@ -0,0 +1,79 @@
+const Header1 = document.getElementById('Header1');
+const Text = document.getElementById('Text');
+const Image1 = document.getElementById('Image1');
+
+const Value = document.getElementById('Value');
+const Price = document.getElementById('Price');
+const Max_Speed = document.getElementById('Max_Speed');
+const Estimated_Copies = document.getElementById('Estimated_Copies');
+const Demand = document.getElementById('Demand');
+
+let Data = [];
+
+let ItemNumber = parseInt(document.getElementById("Number").getAttribute("value"));
+
+console.log("Loading information for " + ItemNumber)
+
+const loadData = async () => {
+
+    fetch('https://opensheet.elk.sh/1T9-q3RWbYz_m9g5RPygdAC4vod09XJIRUcN0ijkuSts/1')
+    .then(result => result.json())
+    .then((output) => {
+
+        console.log('Output: ', output);
+
+        Data = output[ItemNumber];
+
+        displayInformation();
+        
+    }).catch(err => console.error(err));
+
+};
+
+const displayInformation = () => {
+
+    console.log(Data)
+
+    const htmlValue = ("Value: " + Data.Value)
+
+    Value.innerHTML = htmlValue;
+    
+
+    const htmlPrice =  ("Price: " + Data.Price)
+
+    Price.innerHTML = htmlPrice;
+
+
+    const htmlMaxSpeed = ("Max Speed: " + Data.MaxSpeed)
+
+    Max_Speed.innerHTML = htmlMaxSpeed;
+
+
+    const htmlCopies= ("Estimated Rarity: " + Data.Rarity)
+    
+    Estimated_Copies.innerHTML = htmlCopies;
+
+
+    const htmlDemand= ("Demand: " + Data.Demand)
+
+    Demand.innerHTML = htmlDemand;
+
+
+
+    const htmlString = Data.Header1
+
+    Header1.innerHTML = htmlString;
+
+
+    const htmlString2 = `<img style="min-width : 16rem; height : 100%; display: block; margin-left: auto; margin-right: auto;" src="${Data.Image1}" alt="Brulee_Image">`
+
+    Image1.innerHTML = htmlString2;
+
+
+    const htmlString3 = Data.Text
+
+    Text.innerHTML = htmlString3;
+
+};
+
+loadData();
--- scripts/vehiclesearch.js
+++ scripts/vehiclesearch.js
@@ -1,34 +1,45 @@
 const charactersList = document.getElementById('list');
 const searchBar = document.getElementById('searchBar');
 
-let hpCharacters = [
-    {"name": "Brulee", "value": "5.8M" ,"image": '/media/images/Brulee_Icon.png', "link": "/vehicles/brulee"},
-    {"name": "Beignet", "value": "3.5M" ,"image": '/media/images/Beignet_Icon.png', "link": "/vehicles/beignet"},
-    {"name": "M12 Molten", "value": "3.6M" ,"image": '/media/images/Molten_Icon.png', "link": "/vehicles/m12molten"},
-];
-
+let Data = [];
 
 searchBar.addEventListener('keyup', (e) => {
     const searchString = (e.target.value.toLowerCase()).trim();
 
-    const filteredCharacters = hpCharacters.filter((character) => {
+    const filteredCharacters = Data.filter((character) => {
         return (
-            character.name.toLowerCase().includes(searchString)
+            character.Name.toLowerCase().includes(searchString)
         );
     });
     displayCharacters(filteredCharacters);
 });
+
+const loadData = async () => {
+
+    fetch('https://opensheet.elk.sh/1T9-q3RWbYz_m9g5RPygdAC4vod09XJIRUcN0ijkuSts/1')
+    .then(result => result.json())
+    .then((output) => {
+
+        console.log('Output: ', output);
+
+        Data = output;
+
+        displayCharacters(Data);
+        
+    }).catch(err => console.error(err));
+
+};
 
 const displayCharacters = (characters) => {
     const htmlString = characters
         .map((character) => {
             return `
             <div class="selectionbox">
-                <a href="${character.link}">
+                <a href="${character.Link}">
 
-                    <div class="text" style="font-size: 2.5rem; font-weight: 600;">${character.name}</div>
-                    <img id="selectionimage" src="${character.image}" />
-                    <div class="text" style="font-size: 2rem; font-weight: 600; color: rgb(0, 255, 0);">Value: ${character.value}</div>
+                    <div class="text" style="font-size: 2.5rem; font-weight: 600;">${character.Header1}</div>
+                    <img id="selectionimage" src="${character.Image1}" />
+                    <div class="text" style="font-size: 2rem; font-weight: 600; color: rgb(0, 255, 0);">Value: ${character.Value2}</div>
 
                 </a>
             </div>
@@ -38,4 +49,4 @@ const displayCharacters = (characters) => {
     charactersList.innerHTML = htmlString;
 };
 
-displayCharacters(hpCharacters);
+loadData();
